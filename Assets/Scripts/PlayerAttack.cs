64c1ecf2d3bc8d4f9d587b3f19478a6d744fa17d
Hernan Quintana <hernan@jsatch.com>
2024-09-28 09:49:45 -0500

    Ataque terminado
    Falta respuesta del enemigo.

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -2,14 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField]
     private PlayerMovement m_PlayerMovement;
 
+    [SerializeField]
+    private float m_AttackRange = 0.25f;
+
+    private Transform m_AttackQueryPoint;
+
+    private void Awake() {
+        m_AttackQueryPoint = transform.Find("AttackQueryPoint");
+    }
+
     public void AttackEnd()
     {
-        Debug.Log("AnimationEvent");
         m_PlayerMovement.DeactivateAttack1();
+    }
+
+    public void AttackQuery()
+    {
+        var collider = Physics2D.OverlapCircle(
+            m_AttackQueryPoint.position,
+            m_AttackRange,
+            LayerMask.GetMask("Enemy")
+        );
+        if (collider != null)
+        {
+            // hubo colision
+            Debug.Log("Hubo colision");
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(m_AttackQueryPoint.position, m_AttackRange);
     }
 }
