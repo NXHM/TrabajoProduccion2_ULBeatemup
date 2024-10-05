b40ee70f00c88bf7d0585feb7a9636b6cbcf4941
Hernan <hernan@jsatch.com>
2024-10-04 21:51:22 -0500

    Timeline terminado

---
+++ Assets/Scripts/Cutscenes/CutsceneController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayableDirector m_PlayableDirector;
+    private void Awake() 
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_PlayableDirector.Play();
+        }
+    }
+}
