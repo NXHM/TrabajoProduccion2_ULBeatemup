8e278bc80b75252a0a5b7607bf84b7ea1a22841f
AdriUlima <20200711@aloe.ulima.edu.pe>
2024-10-14 20:04:55 -0500

    PlayerCorregido

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,84 +15,105 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float m_JumpSpeed = 350f;
-    public bool IsJumping = false;
-    private bool m_CanAttack = true;
+    //public bool IsJumping = false;
+    //private bool m_CanAttack = true;
 
-    [SerializeField]
-    private FloorMovement m_FloorMovement;
+    //[SerializeField]
+    //private FloorMovement m_FloorMovement;
 
-    [SerializeField]
-    private Transform m_RaycastPoint;
+    //[SerializeField]
+    //private Transform m_RaycastPoint;
     [SerializeField]
     private float m_RaycastDistance = 0.1f;
 
-    private Vector3 m_InitialPos;
+    //private Vector3 m_InitialPos;
 
-    private Rigidbody2D m_SpriteRb;
+    //private Rigidbody2D m_SpriteRb;
     private Vector2 m_Velocity = Vector2.zero;
     private AudioSource m_AudioSource;
 
     private Animator m_SpriteAnimator;
 
+    private enum Actions
+    {
+        Walking, Jumping, Attacking
+    }
+    private Actions action;
+    private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rbSalto;
+    private Transform playerSprite;
+
     private void Awake()
     {
-        m_SpriteRb = transform.Find("Sprite").GetComponent<Rigidbody2D>();
-        m_SpriteAnimator = transform.Find("Sprite").GetComponent<Animator>();
+        //m_SpriteRb = transform.Find("PlayerSprite").GetComponent<Rigidbody2D>();
+        playerSprite = transform.Find("PlayerSprite");
+        m_SpriteAnimator = playerSprite.GetComponent<Animator>();
         m_AudioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
     {
-        m_InitialPos = transform.position;
+        //m_InitialPos = transform.position;
     }
 
     public void Move(float movX, float movY)
     {
-        if (movX < 0 && m_IsFacingRight)
+        if (action != Actions.Attacking)
         {
-            transform.rotation *= Quaternion.Euler(0f, 180f, 0f);
-            m_IsFacingRight = !m_IsFacingRight;
-        }
-        if (movX > 0 && !m_IsFacingRight)
-        {
-            transform.rotation *= Quaternion.Euler(0f, -180f, 0f);
-            m_IsFacingRight = !m_IsFacingRight;
-        }
+            if (movX < 0 && m_IsFacingRight)
+            {
+                transform.rotation *= Quaternion.Euler(0f, 180f, 0f);
+                m_IsFacingRight = !m_IsFacingRight;
+            }
+            if (movX > 0 && !m_IsFacingRight)
+            {
+                transform.rotation *= Quaternion.Euler(0f, -180f, 0f);
+                m_IsFacingRight = !m_IsFacingRight;
+            }
 
-        m_Velocity = new Vector2(
-            movX * m_SpeedX,
-            movY * m_SpeedY
-        );
+            m_Velocity = new Vector2(
+                movX * m_SpeedX,
+                movY * m_SpeedY
+            );
 
-        if (Mathf.Abs(movX) > 0f)
-        {
-            m_SpriteAnimator.SetFloat("MovX", Mathf.Abs(movX));
-            m_SpriteAnimator.SetBool("Stop", false);
-        } else if (Mathf.Abs(movY) > 0f)
-        {
-            m_SpriteAnimator.SetFloat("MovX", Mathf.Abs(movY));
-            m_SpriteAnimator.SetBool("Stop", false);
+            if (Mathf.Abs(movX) > 0f)
+            {
+                m_SpriteAnimator.SetFloat("MovX", Mathf.Abs(movX));
+                m_SpriteAnimator.SetBool("Stop", false);
+            }
+            else if (Mathf.Abs(movY) > 0f)
+            {
+                m_SpriteAnimator.SetFloat("MovX", Mathf.Abs(movY));
+                m_SpriteAnimator.SetBool("Stop", false);
+            }
+            else
+            {
+                m_SpriteAnimator.SetBool("Stop", true);
+                m_SpriteAnimator.SetFloat("MovX", 0f);
+            }
         }
         else
         {
-            m_SpriteAnimator.SetBool("Stop", true);
-            m_SpriteAnimator.SetFloat("MovX", 0f);
+            m_Velocity = Vector2.zero;
         }
-
     }
 
     private void Update()
     {
-        transform.position += (Vector3)(m_Velocity * Time.deltaTime);
+        //transform.position += (Vector3)(m_Velocity * Time.deltaTime);
+        rb.velocity = m_Velocity;
+        playerSprite.localPosition = rbSalto.position;
     }
 
     private void FixedUpdate()
     {
-        if (IsJumping && m_SpriteRb.velocity.y < 0f)
+        if (action == Actions.Jumping && rbSalto.velocity.y < 0f)
         {
-            m_SpriteAnimator.SetFloat("VelY", m_SpriteRb.velocity.y);
+            m_SpriteAnimator.SetFloat("VelY", rbSalto.velocity.y);
 
-            if (IsGrounded())
+            //if (IsGrounded())
+            if(rbSalto.position.y<=0)
             {
                 Debug.Log("DeactivateJump");
                 DeactivateJump();
@@ -103,7 +124,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump()
     {
-        if (!IsJumping)
+        if (action == Actions.Walking)
         {
             ActivateJump();
         }
@@ -111,7 +132,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Attack1()
     {
-        if (m_CanAttack && !IsJumping)
+        if (action == Actions.Walking)
         {
             ActivateAttack1();
         }
@@ -120,16 +141,21 @@ public class PlayerMovement : MonoBehaviour
     public void ActivateJump()
     {
         Debug.Log("ActivateJump");
-        IsJumping = true;
+        //IsJumping = true;
+        action = Actions.Jumping;
         m_SpriteAnimator.SetFloat("VelY", 0f);
-        m_SpriteRb.AddForce(Vector2.up * m_JumpSpeed);
+        rbSalto.bodyType = RigidbodyType2D.Dynamic;
+        rbSalto.AddForce(Vector2.up * m_JumpSpeed);
         m_SpriteAnimator.SetBool("Jump", true);
         m_AudioSource.Play();
     }
 
     public void DeactivateJump()
     {
-        IsJumping = false;
+        rbSalto.bodyType = RigidbodyType2D.Static;
+        rbSalto.transform.position = Vector3.zero;
+        //IsJumping = false;
+        action = Actions.Walking;
         m_SpriteAnimator.SetBool("Jump", false);
         m_SpriteAnimator.SetFloat("VelY", 0f);
     }
@@ -138,15 +164,17 @@ public class PlayerMovement : MonoBehaviour
     {
         m_AudioSource.Play();
         m_SpriteAnimator.SetTrigger("Attack1");
-        m_CanAttack = false;
+        //m_CanAttack = false;
+        action = Actions.Attacking;
     }
 
     public void DeactivateAttack1()
     {
-        m_CanAttack = true;
+        //m_CanAttack = true;
+        action = Actions.Walking;
     }
 
-    public bool IsGrounded()
+    /*public bool IsGrounded()
     {
         var hit = Physics2D.Raycast(
             m_RaycastPoint.position,
@@ -159,12 +187,12 @@ public class PlayerMovement : MonoBehaviour
             return true;
         }
         return false;
-    }
+    }*/
 
-    public void OnDrawGizmos()
+    /*public void OnDrawGizmos()
     {
         Gizmos.DrawRay(
             m_RaycastPoint.position,
             Vector3.down * m_RaycastDistance);
-    }
+    }*/
 }
