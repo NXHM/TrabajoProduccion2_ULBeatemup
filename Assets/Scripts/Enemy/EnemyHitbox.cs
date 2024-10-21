a3c0a00a54a229697af856994d35ce0ac56deaf0
Jazmin Naomi Cardenas <114670107+JAZZ240S@users.noreply.github.com>
2024-10-21 16:26:08 -0500

    finalfixx

--- Assets/Scripts/Enemy/EnemyHitbox.cs
+++ Assets/Scripts/Enemy/EnemyHitbox.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
-
+// enemyhitbox
 public class EnemyHitbox : MonoBehaviour
 {
     [SerializeField]
