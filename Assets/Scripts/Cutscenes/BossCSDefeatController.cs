dbaa0e925a1090bb06b7a0ca1598e63ec43e3e9b
Carlos Quiñones Costa <carlosquinonescosta2003@gmail.com>
2024-10-21 06:42:37 -0500

    Boss cinematics

---
+++ Assets/Scripts/Cutscenes/BossCSDefeatController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class BossCSDefeatController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayableDirector m_PlayableDirector;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_PlayableDirector.Play();
+            gameObject.SetActive(false);
+        }
+    }
+}
